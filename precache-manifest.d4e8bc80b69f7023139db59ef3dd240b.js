34e2116da707a2d626c5ec3995b6c46d3f31c09e
Naum Jefferson <naum.jefferson@recife.pe.gov.br>
2020-02-21 12:41:00 -0300

    Updates

---
+++ precache-manifest.d4e8bc80b69f7023139db59ef3dd240b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b889e1687d4e62d5092b2872ccbb28e0",
+    "url": "/teste-deploy-react/index.html"
+  },
+  {
+    "revision": "e9d1e9ba63067ca14b83",
+    "url": "/teste-deploy-react/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "44ba651025a63f6e2723",
+    "url": "/teste-deploy-react/static/js/2.cffeed26.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/teste-deploy-react/static/js/2.cffeed26.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e9d1e9ba63067ca14b83",
+    "url": "/teste-deploy-react/static/js/main.d63f1d9e.chunk.js"
+  },
+  {
+    "revision": "1cc4ec6ce3a37712dbb9",
+    "url": "/teste-deploy-react/static/js/runtime-main.f3708a5e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/teste-deploy-react/static/media/logo.5d5d9eef.svg"
+  }
+]);
